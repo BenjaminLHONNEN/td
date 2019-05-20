867dc0a8ee1102ec9f405040388e9bfef3620ba6
Mathieu TUDISCO <oss@mathieutu.dev>
2019-05-20 16:22:54 +0200

    Update MyFirstAsyncIO.js

--- learnyounode/MyFirstAsyncIO.js
+++ learnyounode/MyFirstAsyncIO.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-/*fs.readFile(process.argv[2], function (err, buffer) {
+fs.readFile(process.argv[2], function (err, buffer) {
     console.log(buffer.toString().split('\n').length - 1);
-});*/
+});
 
 /*
 fs.promises.readFile(process.argv[2]).then((buffer) => {
@@ -10,10 +10,12 @@ fs.promises.readFile(process.argv[2]).then((buffer) => {
 });
 */
 
-async function test() {
+/*
+async function withAsyncAwait() {
     const buffer = await fs.promises.readFile(process.argv[2]);
     console.log(
         buffer.toString().split('\n').length - 1
     );
 }
-test();
+withAsyncAwait();
+*/
